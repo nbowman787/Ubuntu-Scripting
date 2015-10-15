6fb2593a3a398e69b8a18eb7d23d96f368f0c75a
nbowman787 <n.bowman787@gmail.com>
2015-10-15 15:19:00 +0000

    changes done to varexamples done by nathan bowman 10/15/2015

--- varexamples.sh
+++ varexamples.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/bin/bash -x
 # this script creates an array 
 
 colours=(red green blue)
 
 declare -A animals
 animals=([red]="cardinal" [green]="frog" [blue]="lobster")
+
+echo "the colours array contains ${colours[0]}, ${colours[1]}, ${colours[2]}
+echo "the array colours has the following content: ${colours[@]}
